f0baa3fa8101ab4186819d977f797ce49140a92d
jessepincay <pincayjesse@gmail.com>
2024-07-13 23:35:44 -0500

    Revert "sm8350-common: Import powerhint.json from lahaina"
    This reverts commit 2c0c8cdf14d32a395effec2e5f2dce0a0feb1e69.

--- common.mk
+++ common.mk
@@ -465,9 +465,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     libqti-perfd-client
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
 PRODUCT_PACKAGES += \
     libssl.vendor
 
