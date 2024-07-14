99b819dacdec31f3d7b07a3e353b6ecdb0bd4b2b
jessepincay <pincayjesse@gmail.com>
2024-07-13 20:03:37 -0500

    Revert " Set PRODUCT_ENABLE_UFFD_GC to "true""
    This reverts commit 21642ac44d38123c6b92fb1fbc5e4c8023e509ad.

--- common.mk
+++ common.mk
@@ -334,7 +334,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
 # Kernel
-PRODUCT_ENABLE_UFFD_GC := true
+PRODUCT_ENABLE_UFFD_GC := false
 
 # Keymaster
 PRODUCT_PACKAGES += \
